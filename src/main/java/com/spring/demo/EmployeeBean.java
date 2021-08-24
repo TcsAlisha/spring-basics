38df3e6e096c649d0cc4be79ee2a812f5a30a893
TcsAlisha <tcsalishadias@gmail.com>
2021-08-24 05:47:19 +0000

    first commit

---
+++ src/main/java/com/spring/demo/EmployeeBean.java
@@ -0,0 +1,19 @@
+package com.spring.demo;
+
+public class EmployeeBean {
+	private String fullName;
+	private DepartmentBean departmentBean ;//= new DepartmentBean();
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public DepartmentBean getDepartmentBean() {
+		return departmentBean;
+	}
+	public void setDepartmentBean(DepartmentBean departmentBean) {
+		this.departmentBean = departmentBean;
+	}
+	
+}
